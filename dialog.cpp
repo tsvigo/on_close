9679ccb42bb8a10f3f99c0dca35cbaf24b5b029b
Viktor <11_70@bk.ru>
2024-04-19 07:26:58 +0300

    Описание коммита

--- dialog.cpp
+++ dialog.cpp
@@ -1,15 +1,71 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \brief Dialog::Dialog
+/// \param parent
+///.
+#include <QApplication>                                                                                               
+#include <QPushButton>                                                                                                
+#include <QtDebug>                                                                                                    
+#include <signal.h>  
+using namespace std;
+#include <fstream>
+#include <iostream>
+#include <QGuiApplication>
+#include <signal.h>  
+///
+/// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// .
+/// 
+//void signalhandler(int sig){
+//  if(sig==SIGINT){
+//  //  qApp->quit();
+//   std::cout << "тест"// stderr.toStdString().c_str()
+//     << std::endl; 
+//  //   exit(0   );
+//  }
+//}
+ 
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// ./ 
 Dialog::Dialog(QWidget *parent)
      : QDialog(parent)
      , ui(new Ui::Dialog)
      {
           ui->setupUi(this);
+         // qApp()->setQuitOnLastWindowClosed(false);
+         QApplication::setQuitOnLastWindowClosed(false);
+         //  signal(SIGINT, signalhandler);
      }
 
 Dialog::~Dialog()
      {
           delete ui;
      }
+
+//void MyDialog::reject()
+//{
+//    QMessageBox::StandardButton resBtn = QMessageBox::Yes;
+//    if (changes) {
+//        resBtn = QMessageBox::question( this, APP_NAME,
+//                                        tr("Are you sure?\n"),
+//                                        QMessageBox::Cancel | QMessageBox::No | QMessageBox::Yes,
+//                                        QMessageBox::Yes);
+//    }
+//    if (resBtn == QMessageBox::Yes) {
+//        QDialog::reject();
+//    }
+//}
+
+
+void Dialog::on_pushButton_clicked()
+{
+    QApplication::setQuitOnLastWindowClosed(true);
+ //   signal(sig, SIG_IGN);
+// SIGQUIT;
+  //  exit(0   );
+//     signal(SIGINT, signalhandler);
+}
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,92 @@
 #include "dialog.h"
 
 #include <QApplication>
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \brief Dialog::Dialog
+/// \param parent
+///.
+                                                                                             
+#include <QPushButton>                                                                                                
+#include <QtDebug>                                                                                                    
+#include <signal.h>  
+using namespace std;
+#include <fstream>
+#include <iostream>
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <QCoreApplication>
+
+#include <initializer_list>
+
+#include <unistd.h>
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// \brief The Button class
+///. 
+//class Button : public QPushButton {
+//  Q_OBJECT
+//public slots:
+//  void doSomething() { qDebug() << "About to quit!"; }
+//};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void ignoreUnixSignals(std::initializer_list<int> ignoreSignals) {
+    // all these signals will be ignored.
+    for (int sig : ignoreSignals)
+        signal(sig, SIG_IGN);
+}
+
+void catchUnixSignals(std::initializer_list<int> quitSignals) {
+    auto handler = [](int sig) -> void {
+        // blocking and not aysnc-signal-safe func are valid
+        printf("\nquit the application by signal(%d).\n", sig);
+        // Здесь обработка функций
+        QCoreApplication::quit();
+    };
+    
+    sigset_t blocking_mask;   
+    sigemptyset(&blocking_mask);  
+    for (auto sig : quitSignals) 
+        sigaddset(&blocking_mask, sig);  
+        
+    struct sigaction sa;   
+    sa.sa_handler = handler;   
+    sa.sa_mask    = blocking_mask;  
+    sa.sa_flags   = 0;    
+    
+    for (auto sig : quitSignals)   
+        sigaction(sig, &sa, nullptr);
+}
+
+/// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// .
+/// 
+//void signalhandler(int sig){
+//  if(sig==SIGINT){
+//  //  qApp->quit();
+//   std::cout << "тест"// stderr.toStdString().c_str()
+//     << std::endl; 
+//    QApplication::beep();
+//  }
+//}
+/// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// . 
 int main(int argc, char *argv[])
      {
           QApplication a(argc, argv);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+          /// \brief w
+    catchUnixSignals({SIGQUIT, SIGINT, SIGTERM, SIGHUP, SIGKILL});
+
+    // do your initialization and other stuff
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+          /// \brief w
+          ///.         
           Dialog w;
           w.show();
-          return a.exec();
+           //  std::cout << "тест"     << std::endl; 
+       //  QApplication::setQuitOnLastWindowClosed(false);
+         // signal(SIGINT, signalhandler);
+         return a.exec();
      }
+
